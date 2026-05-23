d988530c2e682efd302daf44b93dad7598556926
maheeen-khan <maheeenkhan4@gmail.com>
2026-05-23 10:35:49 +0500

    update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -34,7 +34,10 @@ const TEST_EMAIL = process.env.SMTP_USER
 const app = express()
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://advace-fullstack-blog-app-g1tb-992piknaa-unshakhans-projects.vercel.app'],
+  origin: [
+    'http://localhost:5173',
+    'https://advace-fullstack-blog-app-g1tb.vercel.app'
+  ],
   credentials: true
 }))
 app.use(express.json())
